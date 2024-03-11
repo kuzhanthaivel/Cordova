493df375c86946b044e4ea88cd3377a9385f8977
kuzhanthaivel <kuzhanthaivel272@gmail.com>
2024-03-11 23:27:44 +0530

    cordva installation

---
+++ MyApp/www/js/index.js
@@ -0,0 +1,13 @@
+function saveInfo() {
+    var name = document.getElementById("name").value;
+    var regNo = document.getElementById("regNo").value;
+    var phone = document.getElementById("phone").value;
+    var collegeNo = document.getElementById("collegeNo").value;
+
+    var info = "Name: " + name + "<br>" +
+               "Registration Number: " + regNo + "<br>" +
+               "Phone Number: " + phone + "<br>" +
+               "College Number: " + collegeNo;
+
+    document.getElementById("displayInfo").innerHTML = info;
+}
